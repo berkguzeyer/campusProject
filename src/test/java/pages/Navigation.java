d0ae8070bf260b6b559079a1b964c71caa623173
berkg <berkguzeyer@yahoo.com>
2023-04-08 12:46:39 -0400

    Created the framework; added utilities package & methods, along with 2 page elements. Initial commit.

---
+++ src/test/java/pages/Navigation.java
@@ -0,0 +1,77 @@
+package pages;
+
+import utilities.DriverClass;
+import utilities.MyMethods;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Navigation extends MyMethods { // always extend from "Pages" so you won't have to create to many objects for MyMethods class
+
+    public Navigation() {
+        PageFactory.initElements(DriverClass.getDriver(), this);
+    }
+
+    @FindBy(xpath = "(//span[text()='Setup'])[1]")
+    private WebElement setupBtn;
+
+    @FindBy(xpath = "//span[text()='Parameters']")
+    private WebElement parameterBtn;
+
+    @FindBy(xpath = "(//span[text()='Countries'])[1]")
+    private WebElement countriesBtn;
+
+    @FindBy(xpath = "//a[@href='/citizenships/list']")
+    private WebElement citizenshipButton;
+
+    @FindBy(xpath = "(//span[text()='Fees'])[1]")
+    private WebElement feesButton;
+
+    @FindBy(xpath = "(//span[text()='Entrance Exams'])[1]")
+    private WebElement entranceExamsButton;
+
+    @FindBy(xpath = "(//span[text()='Setup'])[2]")
+    private WebElement entranceExamsSetupButton;
+
+    @FindBy(xpath = "(//span[text()='Entrance Exams'])[2]")
+    private WebElement entranceExamsInnerButton;
+
+    @FindBy(xpath = "//span[text()='States']")
+    private WebElement statesBtn;
+
+    public WebElement getStatesBtn() {
+        return statesBtn;
+    }
+
+    public WebElement getEntranceExamsButton() {
+        return entranceExamsButton;
+    }
+
+    public WebElement getEntranceExamsSetupButton() {
+        return entranceExamsSetupButton;
+    }
+
+    public WebElement getEntranceExamsInnerButton() {
+        return entranceExamsInnerButton;
+    }
+
+    public WebElement getFeesButton() {
+        return feesButton;
+    }
+
+    public WebElement getCitizenshipButton() {
+        return citizenshipButton;
+    }
+
+    public WebElement getSetupBtn() {
+        return setupBtn;
+    }
+
+    public WebElement getParameterBtn() {
+        return parameterBtn;
+    }
+
+    public WebElement getCountriesBtn() {
+        return countriesBtn;
+    }
+}
